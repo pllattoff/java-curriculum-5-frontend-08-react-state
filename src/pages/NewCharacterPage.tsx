27f770b45a5d2ccb132bd6e3f4b9636e1f9fa618
Stanislav Platov <pllattoff@gmail.com>
2026-08-19 11:40:15 +0200

    add features

--- src/pages/NewCharacterPage.tsx
+++ src/pages/NewCharacterPage.tsx
@@ -1,30 +1,42 @@
-import {FormEvent, useState} from "react";
 import {Character} from "../data/types.ts";
 import {useNavigate} from "react-router-dom";
 import axios from "axios";
+import {useForm} from "react-hook-form";
 
 type NewCharacterPageProps = {
     characters: Character[];
     addCharacter: (newCharacter: Character) => void;
 }
 
+type FormValues = {
+    name: string;
+    species: string;
+    status: string;
+    image: string;
+};
+
 export default function NewCharacterPage(props: Readonly<NewCharacterPageProps>) {
-
-    const [name, setName] = useState<string>("");
-    const [species, setSpecies] = useState<string>("");
-    const [status, setStatus] = useState<string>("");
-    const [image, setImage] = useState<string>("https://media.moemax.com/i/moemax/PIAeLRYyJ6zLOZSMvyILzAQw/?fmt=auto&%24hq%24=&w=1200");
-
     const navigate = useNavigate();
 
-    function handleSubmit(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault();
+    const {
+        register,
+        handleSubmit,
+        formState: {errors, isValid}
+    } = useForm<FormValues>({
+        mode: "onChange",
+        defaultValues: {
+            image: "https://media.moemax.com/i/moemax/PIAeLRYyJ6zLOZSMvyILzAQw/?fmt=auto&%24hq%24=&w=1200"
+        }
+    });
+
+    function onSubmit(data: FormValues) {
+        // event.preventDefault();          The `handleSubmit` function from `react-hook-form` handles preventing the form's default behavior automatically.
 
         const newCharacter: Character = {
             id: generateId(),
-            name,
-            species,
-            status,
+            name: data.name,
+            species: data.species,
+            status: data.status,
             type: "",
             gender: "",
             origin: {
@@ -35,7 +47,7 @@ export default function NewCharacterPage(props: Readonly<NewCharacterPageProps>)
                 name: "Unknown",
                 url: ""
             },
-            image,
+            image: data.image,
             episode: [],
             url: "",
             created: new Date().toISOString()
@@ -43,10 +55,6 @@ export default function NewCharacterPage(props: Readonly<NewCharacterPageProps>)
 
         postCharacter(newCharacter);
         navigate(`/characters/${newCharacter.id}`);
-        // setName("");
-        // setSpecies("");
-        // setStatus("");
-        // setImage("");
     }
 
     function generateId():number {
@@ -58,57 +66,77 @@ export default function NewCharacterPage(props: Readonly<NewCharacterPageProps>)
     }
 
     function postCharacter(character: Character) {
-        axios.post("https://rickandmortyapi.com/api/character", character)
+        axios
+            .post("https://rickandmortyapi.com/api/character", character)
             .then(response => console.log(response.data))
             .catch(error => {
                 console.error("Network/API error:", error);
                 props.addCharacter(character);
-            })
+            });
     }
 
     return(
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <label>
                 Name:
                 <input
                     type="text"
-                    onChange={e => setName(e.target.value)}
-                    value={name}
                     placeholder="Enter name..."
+                    {...register("name", {
+                        required: "Name is required",
+                        minLength: {
+                            value: 2,
+                            message: "Name must contain at least 2 characters"
+                        },
+                        maxLength: {
+                            value: 30,
+                            message: "Name must contain at most 30 characters"
+                        }
+                    })}
                 />
             </label>
+
+            {errors.name && <p>{errors.name.message}</p>}
 
             <label>
                 Species:
                 <input
                     type="text"
-                    onChange={e => setSpecies(e.target.value)}
-                    value={species}
                     placeholder="Enter species..."
+                    {...register("species", {
+                        required: "Species is required"
+                    })}
                 />
             </label>
+
+            {errors.species && <p>{errors.species.message}</p>}
 
             <label>
                 Status:
                 <input
                     type="text"
-                    onChange={e => setStatus(e.target.value)}
-                    value={status}
                     placeholder="Enter status..."
+                    {...register("status", {
+                        required: "Status is required",
+                        validate: value =>
+                            ["Alive", "Dead", "unknown"].includes(value) ||
+                            "Status must be Alive, Dead or unknown"
+                    })}
                 />
             </label>
+
+            {errors.status && <p>{errors.status.message}</p>}
 
             <label>
                 Image:
                 <input
                     type="text"
-                    onChange={e => setImage(e.target.value)}
-                    value={image}
                     placeholder="Enter the link to the image..."
+                    {...register("image")}
                 />
             </label>
 
-            <button type="submit">
+            <button type="submit" disabled={!isValid}>
                 Add
             </button>
         </form>
